0f30676802aa47aba2686594c6f79a5226f682f7
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-08 20:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     if (studentId === '12345' && password === 'password') {
         document.getElementById('message').innerText = 'Welcome, student!';
     } else {
-        document.getElementById('message').innerText = 'Yad aa rhi to roommet se chup k Babu ko call car lo hmm nhi Batai ge/gi kisi Se ';
+        document.getElementById('message').innerText = 'Yad aa rhi to sab se chup k Babu ko call car lo hmm nhi Batai ge/gi kisi Se ';
     }
 });
 
